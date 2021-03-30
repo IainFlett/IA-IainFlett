1d918f37c330ac015e667d99cb03d4b5cc5c551d
IainFlett <iainflett37@gmail.com>
2021-03-30 10:48:17 -0700

    firstIAtest

---
+++ variableone.java
@@ -0,0 +1,16 @@
+public class variableone{
+
+public static void main(String args[]){
+
+int number1;
+int number2;
+
+number1 = 12;
+number2 = 22;
+
+System.out.println(number1 + number2);
+
+
+}
+
+}
