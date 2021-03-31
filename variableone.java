461952850f56e600400e19706add356695021a4a
mr.gill <jgill@sd42.ca>
2021-03-31 10:19:15 -0700

    Update variableone.java
    cause 3 numbers are better than 2

--- variableone.java
+++ variableone.java
@@ -4,11 +4,13 @@ public static void main(String args[]){
 
 int number1;
 int number2;
+int number3;  
 
-number1 = 12;
+number1 = 22;
 number2 = 22;
+number3 = 22;  
 
-System.out.println(number1 + number2);
+System.out.println(number1 + number2 + number3);
 
 
 }
