14020ca328c600a5e623219fb95f20fdfed76469
IainFlett <81584173+IainFlett@users.noreply.github.com>
2021-04-01 09:58:11 -0700

    Update variableone.java

--- variableone.java
+++ variableone.java
@@ -4,13 +4,11 @@ public static void main(String args[]){
 
 int number1;
 int number2;
-int number3;
   
 number1 = 12;
 number2 = 22;
-number3 = 3;
   
-System.out.println(number1 + number2 - number3);
+System.out.println(number1 + number2);
 
 
 }
